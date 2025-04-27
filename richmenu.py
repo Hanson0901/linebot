8908beffa567a749c6affe7dd14478c805d73482
Hanson0901 <101704292+Hanson0901@users.noreply.github.com>
2025-04-27 20:29:54 +0800

    123456

--- richmenu.py
+++ richmenu.py
@@ -6,20 +6,20 @@ line_bot_api = LineBotApi('88Lnj9pvFPA3IZh1fbQ2FDVmEzXPwd1WZ+5DEaGkdjVOQqi0XOuC6
 
 button1 = RichMenuArea(  # 設定 RichMenu 的一個按鈕區域，命名為 "Button 1"，並設定 PostbackAction 動作
     bounds=RichMenuBounds(x=0, y=0, width=1250, height=1686),
-    action=PostbackAction(label='button 1', data='action=input')
+    action=PostbackAction(label="button 1", data="action=input"),
 )
 
 button2 = RichMenuArea(  # 設定 RichMenu 的第二個按鈕區域，命名為 "查詢個人資料"，並設定 MessageAction 動作
     bounds=RichMenuBounds(x=1250, y=0, width=1250, height=1686),
-    action=MessageAction(label='查詢個人資料', text='查詢個人資料')
+    action=MessageAction(label="查詢個人資料", text="查詢個人資料"),
 )
 
 rich_menu = RichMenu(
     size=RichMenuSize(width=2500, height=1686),  # 設定 RichMenu 大小
     selected=True,
-    name='CGU',  # 設定 RichMenu 的名稱
-    chat_bar_text='CGU',  # 設定 RichMenu 在聊天視窗中的顯示文字
-    areas=[button1, button2]  # 設定 RichMenu 的點擊區域
+    name="CGU",  # 設定 RichMenu 的名稱
+    chat_bar_text="CGU",  # 設定 RichMenu 在聊天視窗中的顯示文字
+    areas=[button1, button2],  # 設定 RichMenu 的點擊區域
 )
 
 try:
@@ -30,11 +30,11 @@ try:
     # 上傳 Rich Menu 圖片
     with open('richmenu.png', 'rb') as f:
         line_bot_api.set_rich_menu_image(rich_menu_id, 'image/png', f)
-        print('Rich Menu image uploaded.')
+        print("Rich Menu image uploaded.")
 
     # 設定 Rich Menu 至 Channel
     line_bot_api.set_default_rich_menu(rich_menu_id)
-    print('Rich Menu set as default.')
+    print("Rich Menu set as default.")
 
 except LineBotApiError as e:
-    print(f'Error creating Rich Menu: {e}')
+    print(f"Error creating Rich Menu: {e}")
